c7a823024b621ac496679cc691dab0268642282e
david965154 <145984683+david965154@users.noreply.github.com>
2023-09-25 19:07:34 +0800

    Add files via upload

---
+++ how_leading_zeros_should_kept.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+
+uint16_t count_leading_zeros(uint64_t x)
+{
+    x |= (x >> 1);
+    x |= (x >> 2);
+    x |= (x >> 4);
+    x |= (x >> 8);
+    x |= (x >> 16);
+    x |= (x >> 32);
+
+    /* count ones (population count) */
+    x -= ((x >> 1) & 0x5555555555555555 );
+    x = ((x >> 2) & 0x3333333333333333) + (x & 0x3333333333333333 );
+    x = ((x >> 4) + x) & 0x0f0f0f0f0f0f0f0f;
+    x += (x >> 8);
+    x += (x >> 16);
+    x += (x>>32);
+
+    return (64 - (x & 0x7f));
+}
+
+int main(){
+    uint64_t x,y;
+    scanf("%llu %llu",&x, &y);
+    x = count_leading_zeros(x);
+    y = count_leading_zeros(y);
+    printf("%d",(x>y)?(x-y):(y-x));
+
+    return 0;
+}
